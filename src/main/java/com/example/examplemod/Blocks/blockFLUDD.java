cbb7ab72b0622962c815672132680d257efd7cb5
IroPlisk <loremcrux04@gmail.com>
2023-10-03 17:04:34 +0200

    Initial commit

---
+++ src/main/java/com/example/examplemod/Blocks/blockFLUDD.java
@@ -0,0 +1,16 @@
+/*
+DA FARE POI
+package com.example.mymod;
+
+import net.minecraft.bl.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.ObjectHolder;
+
+public class blockFLUDD extends Block {
+    public MyBlock() {
+        super(Properties.create(Material.ROCK).hardnessAndResistance(2.0f));
+    }
+}
+*/
---
+++ src/main/java/com/example/examplemod/Items/ItemArmorFLUDD.java
@@ -0,0 +1,12 @@
+package com.example.examplemod.Items;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraftforge.items.IItemHandler;
+
+public class ItemArmorFLUDD extends ArmorItem{
+    public ItemArmorFLUDD(ArmorMaterial p_40386_, EquipmentSlot p_40387_, Properties p_40388_) {
+        super(p_40386_, p_40387_, p_40388_);
+    }
+}
